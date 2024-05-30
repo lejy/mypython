bb413a0d9c65f68bd0e22424bd8cd6ed264b6439
lejy <129030474+lejy@users.noreply.github.com>
2024-05-30 20:26:33 +0900

    엘리스코딩2

--- src/문제/test.py
+++ src/문제/test.py
@@ -1,8 +1,18 @@
-example_dict = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4
-}
-for i in range(len(example_dict)):
-    print(example_dict.keys())
+def analyze_text(words):
+    # 키워드, 해시태그, 멘션을 저장할 리스트를 각각 생성합니다.
+    keywords, hashtags, mentions = [], [], []
+
+    for i in words:
+        if i.startswith("@"):
+            mentions.append(i[1:])
+        elif i.startswith("#"):
+            hashtags.append(i[1:])
+        else:
+            keywords.append(i)
+
+    return keywords, hashtags, mentions
+
+
+test=["#hello","@mention","origin"]
+
+print(analyze_text(test))
